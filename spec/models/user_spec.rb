df9af55a555775e6f69c311ad82c86893d7e3175
Barry Lam <blam789@outlook.com>
2014-04-01 10:33:17 -0400

    added association unit tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,32 +2,32 @@ require 'spec_helper'
 
 describe User do
   describe 'validations' do
-    let(:valid_attributes) {{ user_name: 'Batman', email: 'batman@gmail.com', password: '12345678' }}
-    let(:user) { User.new(valid_attributes) }
+    let(:blank) {['', nil]}
+    let(:valid_attributes) {{
+      user_name: 'Batman',
+      email: 'batman@gmail.com',
+      password: '12345678'
+      }}
+    let(:user) {User.new(valid_attributes)}
+
+    it {should have_valid(:user_name).when('Batman')}
+    it {should_not have_valid(:user_name).when(*blank)}
+
+    it {should have_valid(:email).when('batman@gmail.com')}
+    it {should_not have_valid(:email).when(*blank)}
+
+    it {should have_valid(:password).when('12345678')}
+    it {should_not have_valid(:password).when(*blank)}
+    it {should_not have_valid(:password).when('1234')}
 
     it 'is valid when given valid attributes' do
       expect(user).to be_valid
     end
-
-    it 'requires a user name' do
-      user.user_name = ''
-      user.save
-      expect(user).not_to be_valid
-    end
-
-    it 'requires an email' do
-      user.email = ''
-      user.save
-      expect(user).not_to be_valid
-    end
-
-    it 'requires a password' do
-      user.password = ''
-      user.save
-      expect(user).not_to be_valid
-    end
   end
 
   describe 'associations' do
+    it {should have_many(:votes)}
+    it {should have_many(:comments)}
+    it {should have_many(:products)}
   end
 end
