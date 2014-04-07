fe8a235e57de3c105abf0fbede23cc0ce41958c7
Emily Williams <ecwilliams66@gmail.com>
2014-04-07 11:21:24 -0400

    modified files for merge

--- db/migrate/20140402171947_remove_user_name_column_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserNameColumnFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :user_name
-  end
-
-  def down
-    add_column :users, :user_name, :string, null: false
-  end
-end
