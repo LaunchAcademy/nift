b070f5f67076dbb3b26085016c7270368e206e22
Emily Williams <ecwilliams66@gmail.com>
2014-04-09 12:44:39 -0400

    Took out unecessary test

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,7 +5,6 @@ describe Review do
     it { should validate_presence_of(:rating) }
     it { should validate_presence_of(:author_id) }
     it { should have_valid(:rating).when(1, 2, 3, 4, 5) }
-    it { should_not have_valid(:rating).when(0, -3, 6, 5.5, 1.3, 2.4) }
     it 'has a unique author scoped to product' do
       user = FactoryGirl.create(:user)
       FactoryGirl.create(:review, author_id: user.id)
