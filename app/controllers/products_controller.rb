ba945d1d2e4f1338af67820213960940509da6ed
Barry Lam <blam789@outlook.com>
2014-04-13 11:54:27 -0400

    more styling tweaks

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,7 @@ class ProductsController < ApplicationController
     @product.user = current_user
 
     if @product.save
-      redirect_to root_path, notice: "You just created a new product"
+      redirect_to root_path, notice: "Product submitted!"
     else
       render :new
     end
