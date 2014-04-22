b98df471df85e62c50766c3c68aeba51fbc5d0c0
Modsky1 <maude.jake@gmail.com>
2014-04-22 14:28:37 -0400

    comments erased

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Nifty::Application.initialize!
 
-# config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
